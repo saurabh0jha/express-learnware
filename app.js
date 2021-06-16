0438160ad1403b2b1472c57c45c92f09a80a6d8e
Saurabh Ojha <ojha.saurabh81@gmail.com>
2021-06-16 11:16:41 +0530

    adding install scripts in package json

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const _ = require('lodash');
 const arr = [1, [2, [3, [4]]]];
 const flattened = _.flattenDepth(arr, 3);
 console.log(flattened);
-console.log('Hello! The World!');
+console.log('Hello! The World is out there!');
